b1c1a9ce3fadb0e67e299d8be40e3bda26defb36
bnatan91 <75927317+bnatan91@users.noreply.github.com>
2021-12-03 00:20:08 +0800

    register

--- app/src/main/java/id/ac/umn/hidayat/projectuas/register.java
+++ app/src/main/java/id/ac/umn/hidayat/projectuas/register.java
@@ -3,6 +3,7 @@ package id.ac.umn.hidayat.projectuas;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
@@ -10,6 +11,7 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
@@ -29,6 +31,7 @@ public class register extends AppCompatActivity {
     private EditText password;
     private Button register;
     private FirebaseAuth auth;
+    TextView register_text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,34 +45,40 @@ public class register extends AppCompatActivity {
         auth = FirebaseAuth.getInstance();
 
 
+        Button showpassword = findViewById(R.id.showPass);
+        showpassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(password.getTransformationMethod().equals(PasswordTransformationMethod.getInstance())) {
+                    password.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                }
+                else{
+                    password.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                }
+            }
+        });
+
         register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String txt_email = email.getText().toString();
-                String txt_password = password.getText().toString();
+                validation();
 
-                if(TextUtils.isEmpty(txt_email) || TextUtils.isEmpty(txt_password)) {
-                    Toast.makeText(register.this, "Empty credentials", Toast.LENGTH_SHORT).show();
-                } else if(txt_password.length() < 6) {
-                    Toast.makeText(register.this, "Password too short", Toast.LENGTH_SHORT).show();
-                } else {
-                    registerUser(txt_email, txt_password);
-                }
-
-                Button showpassword = findViewById(R.id.showPass);
-                showpassword.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        if(password.getTransformationMethod().equals(PasswordTransformationMethod.getInstance())) {
-                            password.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
-                        }
-                        else{
-                            password.setTransformationMethod(PasswordTransformationMethod.getInstance());
-                        }
-                    }
-                });
             }
         });
+    }
+
+    private void validation(){
+        String txt_email = email.getText().toString();
+        String txt_password = password.getText().toString();
+
+        if(txt_email.isEmpty()) {
+            Toast toast = Toast.makeText(getApplicationContext(), "Enter email address", Toast.LENGTH_SHORT);
+            toast.show();
+        } else if(txt_password.length() < 6 && txt_password.isEmpty()) {
+            Toast.makeText(register.this, "Password too short", Toast.LENGTH_SHORT).show();
+        } else {
+            registerUser(txt_email, txt_password);
+        }
     }
 
     private void registerUser(String email, String password) {
@@ -78,9 +87,22 @@ public class register extends AppCompatActivity {
             public void onComplete(@NonNull Task<AuthResult> task) {
                 if(task.isSuccessful()) {
                     Toast.makeText(register.this, "Registering user successful", Toast.LENGTH_SHORT).show();
+                    Intent user = new Intent(register.this, MainActivity.class);
+                    startActivity(user);
                 } else {
                     Toast.makeText(register.this, "Registration failed", Toast.LENGTH_SHORT).show();
                 }
+            }
+        });
+    }
+
+    public void onClick(View view) {
+        register_text = findViewById(R.id.log);
+        register_text.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent regis = new Intent (register.this, login.class);
+                startActivity(regis);
             }
         });
     }
