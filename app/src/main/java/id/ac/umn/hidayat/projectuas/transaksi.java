a9276865d914a3d14b8616318fcfc4e8a03794d4
EdgarEvert <edgaroscar78@gmail.com>
2021-12-07 04:28:06 +0700

    database

--- app/src/main/java/id/ac/umn/hidayat/projectuas/transaksi.java
+++ app/src/main/java/id/ac/umn/hidayat/projectuas/transaksi.java
@@ -26,17 +26,24 @@ public class transaksi extends AppCompatActivity {
 
     FirebaseDatabase database;
     DatabaseReference myRef;
-    public TextView check_in;
+    private TextView check_in;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transaksi);
 
-        TextView check_in = (TextView) findViewById(R.id.check_in);
+        check_in = findViewById(R.id.check_in);
         TextView check_out = (TextView) findViewById(R.id.check_out);
         TextView total = (TextView) findViewById(R.id.total);
         Button bayar = (Button) findViewById(R.id.bayar);
+
+        Date d2 = Calendar.getInstance().getTime();
+        SimpleDateFormat time = new SimpleDateFormat("HH:mm");
+        SimpleDateFormat hour = new SimpleDateFormat("HH");
+        String hour1 = hour.format(d2);
+        String date3 = time.format(d2);
+        check_out.setText(date3);
 
         getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
         getSupportActionBar().setCustomView(R.layout.activity_back);
@@ -53,20 +60,12 @@ public class transaksi extends AppCompatActivity {
         database  = FirebaseDatabase.getInstance();
         myRef = database.getReference("User");
 
-        
-        Date d2 = Calendar.getInstance().getTime();
-        SimpleDateFormat time = new SimpleDateFormat("HH:mm");
-        SimpleDateFormat hour = new SimpleDateFormat("HH");
-        String hour1 = hour.format(d2);
-        String date3 = time.format(d2);
-        check_out.setText(date3);
+        getdata();
 
         int jam_check_out = Integer.parseInt(hour1);
         // int timeDiff = jam_check_out - jam_check_in;
         // int payment = 3000 + (timeDiff * 2000);
         // total.setText("Rp. "+String.valueOf(payment));
-
-        getdata();
     }
 
     private void getdata() {
