b110e288a53167d02070c5f0eecb853e59a23fc1
EdgarEvert <edgaroscar78@gmail.com>
2021-12-06 16:38:23 +0700

    logout

--- app/src/main/java/id/ac/umn/hidayat/projectuas/MainActivity.java
+++ app/src/main/java/id/ac/umn/hidayat/projectuas/MainActivity.java
@@ -1,5 +1,6 @@
 package id.ac.umn.hidayat.projectuas;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,6 +8,8 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -16,6 +19,8 @@ import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button logout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +28,18 @@ public class MainActivity extends AppCompatActivity {
         LinearLayout qr_code = (LinearLayout) findViewById(R.id.qr_code);
         LinearLayout cameraView = (LinearLayout) findViewById(R.id.camera_view);
         LinearLayout transaksi = (LinearLayout) findViewById(R.id.transaksi);
+
+        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setCustomView(R.layout.activity_logout);
+
+        ImageButton logout = (ImageButton) findViewById(R.id.logout);
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,login.class);
+                startActivity(intent);
+            }
+        });
 
         qr_code.setOnClickListener(new View.OnClickListener() {
             @Override
