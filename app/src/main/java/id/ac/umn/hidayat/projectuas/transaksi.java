2d8fd9fe6562fa09f27b82d5c82ef23920f01469
EdgarEvert <edgaroscar78@gmail.com>
2021-12-07 12:26:23 +0700

    database

--- app/src/main/java/id/ac/umn/hidayat/projectuas/transaksi.java
+++ app/src/main/java/id/ac/umn/hidayat/projectuas/transaksi.java
@@ -47,7 +47,9 @@ public class transaksi extends AppCompatActivity {
 
         TextView check_in = (TextView) findViewById(R.id.check_in);
         check_in.setText(MainActivity.getValue());
-//        String hour2 = check_in;
+//        String jam = String.valueOf(check_in);
+//        String[] parts = jam.split(":");
+//        String jam_checkin = parts[0];
 
         getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
         getSupportActionBar().setCustomView(R.layout.activity_back);
@@ -71,7 +73,8 @@ public class transaksi extends AppCompatActivity {
 //        FirebaseDatabase.getInstance().getReference("User").push().updateChildren(map);
 
         int jam_check_out = Integer.parseInt(hour1);
-//        int jam_check_in = Integer.parseInt(check_in);
+//        int jam_check_in = Integer.parseInt(jam_checkin);
+//        int timeDiff = jam_check_out - jam_check_in;
         int timeDiff = jam_check_out - jam_check_out;
         int payment = 3000 + (timeDiff * 2000);
         total.setText("Rp. "+String.valueOf(payment));
@@ -79,7 +82,7 @@ public class transaksi extends AppCompatActivity {
         bayar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent (transaksi.this,MainActivity.class);
+                Intent intent = new Intent (transaksi.this,login.class);
                 Toast.makeText(transaksi.this, "Pembayaran berhasil!", Toast.LENGTH_SHORT).show();
                 startActivity(intent);
             }
